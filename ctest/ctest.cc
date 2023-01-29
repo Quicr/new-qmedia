0adb221136f1ec52e273f4334bc553a65f0286ae
Cullen Jennings <fluffy@iii.ca>
2023-01-29 07:29:43 -0700

    make ctest subscribe match new API

--- ctest/ctest.cc
+++ ctest/ctest.cc
@@ -1,3 +1,6 @@
+
+#include <iostream>
+
 #include <qmedia/media_client.hh>
 
 //typedef void(CALL *SubscribeCallback)(uint64_t id, uint8_t *data, uint32_t length);
@@ -12,8 +15,7 @@ int main(int argc, char *argv[])
     qmedia::MediaClient subClient("127.0.0.1", 1234, 0, nullptr);
     qmedia::MediaClient pubClient("127.0.0.1", 1234, 0, nullptr);
 
-    subClient.add_audio_stream_subscribe(1,0, subscribe_callback);
-
+    subClient.add_audio_stream_subscribe(1, subscribe_callback);
 
     uint8_t buffer[256];
     pubClient.send_audio_media(1, 
