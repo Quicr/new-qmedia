aa089c1b7795c7cff1bd1072c1b4891af4da0c51
Scott Henning <shenning_00@yahoo.com>
2023-02-22 16:16:23 -0600

    Starting to work on test code.

---
+++ test/media_client_test.cc
@@ -0,0 +1,19 @@
+#include <doctest/doctest.h>
+#include <cstring>
+#include <string>
+#include <iostream>
+#include "qmedia/logger.hh"
+#include "qmedia/media_client.hh"
+
+TEST_CASE("createAndVerify")
+{
+//    LoggerPointer logger = std::make_shared<Logger>("QMediaClient");
+//    logger->SetLogFacility(LogFacility::NOTIFY);
+//
+//    // Create media library.
+//    std::string server = "127.0.0.1";
+//    std::uint16_t port = 14900;
+//    auto client = std::make_unique<qmedia::MediaClient>(server.c_str(), 
+//                       port, quicr::RelayInfo::Protocol::UDP, logger);
+//    CHECK_EQ(client, nullptr);
+}
