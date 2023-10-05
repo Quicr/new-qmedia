b1c34c87d452b10b40e0ea640341c10c2d163607
Richard Barnes <rlb@ipv.sx>
2023-10-05 13:48:09 -0400

    Fix initialization order

--- test/manifest.cpp
+++ test/manifest.cpp
@@ -9,9 +9,9 @@ static const auto manifest_json = std::string(R"(
   "clientId": "d85e3efb-abaa-4f7e-81b3-56f7cbdd4e0d",
   "subscriptions": [{
     "mediaType": "video",
-    "sourceName": "Macbook camera",
+    "sourceName": "Camera A1",
     "sourceId": "80a6ac22-f5a1-406d-bd48-b4c29ff6204b",
-    "label": "Richard B..",
+    "label": "Meeting Participant A",
     "profileSet": {
       "type": "singleordered",
       "profiles": [{
@@ -27,9 +27,9 @@ static const auto manifest_json = std::string(R"(
     }
   }, {
     "mediaType": "audio",
-    "sourceName": "Macbook mic",
+    "sourceName": "Audio A1",
     "sourceId": "68ff975e-17e2-4960-b900-8b74f3e1da85",
-    "label": "Richard B..",
+    "label": "Meeting Participant A",
     "profileSet": {
       "type": "singleordered",
       "profiles": [{
@@ -40,9 +40,9 @@ static const auto manifest_json = std::string(R"(
   }],
   "publications": [{
     "mediaType": "video",
-    "sourceName": "Macbook camera",
+    "sourceName": "Camera A1",
     "sourceId": "80a6ac22-f5a1-406d-bd48-b4c29ff6204b",
-    "label": "Richard B..",
+    "label": "Meeting Participant A",
     "profileSet": {
       "type": "simulcast",
       "profiles": [{
@@ -64,9 +64,9 @@ static const auto manifest_json = std::string(R"(
     }
   }, {
     "mediaType": "audio",
-    "sourceName": "Macbook mic",
+    "sourceName": "Audio A1",
     "sourceId": "68ff975e-17e2-4960-b900-8b74f3e1da85",
-    "label": "Richard B..",
+    "label": "Meeting Participant A",
     "profileSet": {
       "type": "simulcast",
       "profiles": [{
@@ -83,10 +83,12 @@ static const auto manifest_json = std::string(R"(
 })");
 
 static const auto expected_manifest_obj = Manifest{
+    .urlTemplates = {"quicr://webex.cisco.com<pen=1><sub_pen=1>/conferences/<int24>/mediatype/<int8>/endpoint/"
+                     "<int16>"},
     .subscriptions = {{.mediaType = "video",
-                       .sourceName = "Macbook camera",
+                       .sourceName = "Camera A1",
                        .sourceId = "80a6ac22-f5a1-406d-bd48-b4c29ff6204b",
-                       .label = "Richard B..",
+                       .label = "Meeting Participant A",
                        .profileSet = {.type = "singleordered",
                                       .profiles = {{.qualityProfile = "h264,width=1920,height=1080,fps=30,br=2000",
                                                     .quicrNamespaceUrl = "quicr://webex.cisco.com/conferences/13/"
@@ -98,17 +100,17 @@ static const auto expected_manifest_obj = Manifest{
                                                     .quicrNamespaceUrl = "quicr://webex.cisco.com/conferences/13/"
                                                                          "mediatype/194/endpoint/1"}}}},
                       {.mediaType = "audio",
-                       .sourceName = "Macbook mic",
+                       .sourceName = "Audio A1",
                        .sourceId = "68ff975e-17e2-4960-b900-8b74f3e1da85",
-                       .label = "Richard B..",
+                       .label = "Meeting Participant A",
                        .profileSet = {.type = "singleordered",
                                       .profiles = {{.qualityProfile = "opus,br=6",
                                                     .quicrNamespaceUrl = "quicr://webex.cisco.com/conferences/13/"
                                                                          "mediatype/1/endpoint/1"}}}}},
     .publications = {{.mediaType = "video",
-                      .sourceName = "Macbook camera",
+                      .sourceName = "Camera A1",
                       .sourceId = "80a6ac22-f5a1-406d-bd48-b4c29ff6204b",
-                      .label = "Richard B..",
+                      .label = "Meeting Participant A",
                       .profileSet = {.type = "simulcast",
                                      .profiles = {{.qualityProfile = "h264,width=1920,height=1080,fps=30,br=2000",
                                                    .quicrNamespaceUrl = "quicr://webex.cisco.com/conferences/13/"
@@ -126,17 +128,15 @@ static const auto expected_manifest_obj = Manifest{
                                                    .expiry = 500,
                                                    .priorities = {2, 3}}}}},
                      {.mediaType = "audio",
-                      .sourceName = "Macbook mic",
+                      .sourceName = "Audio A1",
                       .sourceId = "68ff975e-17e2-4960-b900-8b74f3e1da85",
-                      .label = "Richard B..",
+                      .label = "Meeting Participant A",
                       .profileSet = {.type = "simulcast",
                                      .profiles = {{.qualityProfile = "opus,br=6",
-                                                   .expiry = 500,
-                                                   .priorities = {1},
                                                    .quicrNamespaceUrl = "quicr://webex.cisco.com/conferences/13/"
-                                                                        "mediatype/1/endpoint/1"}}}}},
-    .urlTemplates = {"quicr://webex.cisco.com<pen=1><sub_pen=1>/conferences/<int24>/mediatype/<int8>/endpoint/"
-                     "<int16>"}};
+                                                                        "mediatype/1/endpoint/1",
+                                                   .expiry = 500,
+                                                   .priorities = {1}}}}}}};
 
 TEST_CASE("Manifest parsing")
 {
