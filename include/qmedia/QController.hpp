7438b6ba2a41ebcb5d69708abd28612877925e9e
Rich Logan <rich@richlogan.co.uk>
2024-02-21 17:26:59 +0000

    Add get/set subscription state

--- include/qmedia/QController.hpp
+++ include/qmedia/QController.hpp
@@ -65,6 +65,8 @@ public:
     std::vector<quicr::Namespace> getSubscriptions(const std::string& sourceId);
     std::vector<PublicationReport> getPublications();
     void setPublicationState(const quicr::Namespace& quicrNamespace, const PublicationState);
+    void setSubscriptionState(const quicr::Namespace& quicrNamespace, const quicr::TransportMode);
+    quicr::SubscriptionState getSubscriptionState(const quicr::Namespace& quicrNamespace);
 private:
     struct PublicationDetails
     {
